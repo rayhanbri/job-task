fb2b7cebf03b8d96db21d3e88ee540f0af610b77
rayhan <mohammadrayhanx0@gmail.com>
2025-08-22 01:21:14 +0600

    mongodb Connected

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,10 @@
+import dbConnect from '@/lib/mongo'
 import React from 'react'
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
+    const  serviceCollection= dbConnect("products");
+    const data =await serviceCollection.find({}).toArray();
+    console.log("helllo bhai",data)
   return (
     <div>i am products</div>
   )
