4eac4d5a36f2e3048d0a9beb90e4eae068252a52
rayhan <mohammadrayhanx0@gmail.com>
2025-08-22 01:45:31 +0600

    showing products

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,9 +3,25 @@ import React from 'react'
 
 export default async function ProductsPage() {
     const  serviceCollection= dbConnect("products");
-    const data =await serviceCollection.find({}).toArray();
-    console.log("helllo bhai",data)
+    const products =await serviceCollection.find({}).toArray();
+    // console.log("helllo bhai",data)
   return (
-    <div>i am products</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+      {products.map((product, index) => (
+        <div key={index} className="card bg-base-200 w-96 shadow-sm">
+          <figure>
+            <img src={product.image} alt={product.name} />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{product.name}</h2>
+            <p>{product.description}</p>
+            <p className="font-semibold">Price: ${product.price}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary w-full ">Read More</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
